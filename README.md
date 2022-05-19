d4e67e455999824dfa131a469524bcce716cb572
MSchmidt951 <m.c.schmidt951@gmail.com>
2022-05-19 16:44:46 +0100

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Multi-Purpose-Controller
-A wireless controller that is designed to be used with multiple devices and projects
+# Multi Purpose Controller
+
+This is a wireless controller that can be used with multiple devices and projects.  
+It uses an ATmega644 to control the circuit and an nRF24LO1 for communication.  
+
+## Features
+
+Controller inputs
+- 6 general purpose buttons
+- 2 general purpose switches
+- 2 joysticks (with built in buttons)
+- 2 trigger buttons
+- A potentiometer
+- An abort/shutdown button
+- Standby mode switch
+- A light button and switch
+
+UART connector for addons
+
+An RGB LED (PL9823) giving the status of the controller
+
+## Usage
+
+1. Turn the power switch on and wait for the green light
+2. Turn on the device that is being used
+3. Press some buttons!
+
+LED colour | Meaning
+---------- | ------------------------
+Red        | Initialising or aborted
+Orange     | Waiting for other device
+Blue       | Standby mode
+Green      | Sending data
+
+## Installation and Setup
+
+### Making the controller
+
+Solder the circuit according to the circuit diagram
+![Circuit Diagram](images/circuitdiagram.png)
+
+An example of a completed circuit (missing triggers) that is compatible with the prints  
+<img src="images/perfboard.jpg" alt="Soldered Circuit" width="828" />
+
+3D print the base of the controller
+
+Attach the electronics to the print with screws
+
+### Uploading the code
+
+Install the [RF24](https://github.com/nRF24/RF24) and [FastLED](https://github.com/FastLED/FastLED) libraries and [MightyCode](https://github.com/MCUdude/MightyCore) on the arduino IDE
+
+Make sure the standard pinout and internal clock is selected in the settings then upload the code to the ATmega644
---
+++ controller/controller.ino
@@ -0,0 +1,152 @@
+#include <RF24.h>
+#include <FastLED.h>
+
+RF24 radio(2, 30);
+byte addresses[][6] = {"C","D"};
+
+CRGB led[1];
+const int LEDbrightness = 45;
+bool aborted = false;
+
+//Input pins
+const int ABORT = 14;
+const int Standby = 13;
+const int lightBtn = 12;
+const int lightSwitch = 11;
+const int switches[3] = {4, 3, 0}; //L, M, R
+const int triggers[2] = {10, 1}; //L, R
+
+const int dirBtns[4] = {15, 21, 20, 22}; //L, R, U, D
+const int btns[2] = {23, 29}; //L, R
+const int pot = 31;
+
+const int StickL[3] = {27, 28, 19}; //x, y, JstickBtnL
+const int StickR[3] = {25, 24, 26}; //r, z, JstickBtnR
+
+//Data bytes
+byte btnByte; //0-3:directional buttons, 4-5:misc buttons, 6-7:triggers
+byte miscByte; //0:abort, 1:standby, 2:light, 3-5:switches, 6-7:JstickBtns
+char sendData[7]; //[x, y, r, z, pot, btnByte, miscByte]
+
+//Functions
+
+void setLED(int r, int g, int b){
+  fill_solid(led, 1, CRGB(r, g, b));
+  FastLED.show();
+}
+
+int toInt(float f){
+  return int(f + 0.5);
+}
+
+
+void setup(){
+  //Set up controls
+  pinMode(ABORT, INPUT_PULLUP);
+  pinMode(Standby, INPUT_PULLUP);
+  pinMode(lightBtn, INPUT_PULLUP);
+  pinMode(lightSwitch, INPUT_PULLUP);
+  for (int i=1; i<3; i++){
+    pinMode(switches[i], INPUT_PULLUP);
+  }
+  for (int i=0; i<2; i++){
+    pinMode(triggers[i], INPUT_PULLUP);
+  }
+  
+  for (int i=0; i<4; i++){
+    pinMode(dirBtns[i], INPUT_PULLUP);
+  }
+  for (int i=0; i<4; i++){
+    pinMode(btns[i], INPUT_PULLUP);
+  }
+  pinMode(pot, INPUT_PULLUP);
+  
+  for (int i=0; i<2; i++){
+    pinMode(StickL[i], INPUT);
+    pinMode(StickR[i], INPUT);
+  }
+
+  //Set up LED
+  delay(2000);
+  FastLED.addLeds<WS2812, 18>(led, 1);
+  
+  //Set up communication
+  setLED(150, 0, 0);
+  delay(1000);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(124);
+
+  //Open pipe
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
+  radio.startListening();
+
+  //Waits until device is on
+  setLED(80, 18, 2);
+  bool Ready = false;
+  while (!Ready){
+    if (radio.available()) {
+      while (radio.available()) {
+        radio.read(&miscByte, sizeof(byte));
+      }
+      Ready = miscByte > 0;
+    }
+    delay(2);
+  }
+  radio.stopListening();
+  setLED(0, LEDbrightness, 0);
+}
+
+void loop(){
+  //Set the LED
+  if (!aborted) {
+    if (!digitalRead(Standby)){
+      setLED(0, 0, LEDbrightness);
+      aborted = false;
+    } else if (!digitalRead(ABORT)) {
+      setLED(LEDbrightness, 0, 0);
+      aborted = true;
+    } else {
+      setLED(0, LEDbrightness, 0);
+    }
+  }
+  
+  /* Get input */
+  //Get joystick input, it is put between 0-254
+  sendData[0] = toInt(analogRead(StickL[0])/4.03); //x
+  sendData[1] = toInt(analogRead(StickL[1])/4.03); //y
+  sendData[2] = toInt(analogRead(StickR[0])/4.03); //r
+  sendData[3] = toInt(analogRead(StickR[1])/4.03); //z
+  //Get potentiometer input, it is put between 0-255
+  sendData[4] = toInt(analogRead(pot)/4.01);
+  
+  //Get button inputs
+  for (int i=0; i<4; i++){
+    bitWrite(btnByte, i, !digitalRead(dirBtns[i]));
+  }
+  bitWrite(btnByte, 4, !digitalRead(btns[0]));
+  bitWrite(btnByte, 5, !digitalRead(btns[1]));
+  bitWrite(btnByte, 6, !digitalRead(triggers[0]));
+  bitWrite(btnByte, 7, !digitalRead(triggers[1]));
+  sendData[5] = btnByte;
+  
+  //Get inputs for miscByte
+  bitWrite(miscByte, 0, !digitalRead(ABORT));
+  bitWrite(miscByte, 1, !digitalRead(Standby));
+  if (!digitalRead(lightSwitch)) {
+    bitWrite(miscByte, 2, digitalRead(lightBtn));
+  } else {
+    bitWrite(miscByte, 2, !digitalRead(lightBtn));
+  }
+  for (int i=1; i<3; i++){
+    bitWrite(miscByte, i+3, !digitalRead(switches[3]));
+  }
+  bitWrite(miscByte, 6, !digitalRead(StickL[2]));
+  bitWrite(miscByte, 7, !digitalRead(StickR[2]));
+  sendData[6] = miscByte;
+
+  /* Send data */
+  radio.write(&sendData, sizeof(char[7]));
+}
